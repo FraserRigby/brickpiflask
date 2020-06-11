5404f7807a960a41dc24e80d948f1181cb9b0e39
FraserRigby <rigbyf20@gmail.com>
2020-06-11 12:56:39 +1000

    commit

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,9 +1,19 @@
 // Home page js
 
-//SignUp popup js
+//Variables
 var modal = document.getElementById('signup');
+var login_data = {};
+
+//SignUp popup js
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+//Login submit function, when login submit button pressed data saved to database
+function user_login_submit() {
+    login_data[username] = document.getElementById('login_username').value
+    login_data[password] = document.getElementById('login_password').value
+    JSONrequest('/login', 'POST')
 }
