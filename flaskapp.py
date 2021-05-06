350966fb5ebdcf53713d58704fdea75834a9e9d4
Frase Rigby <rigbyf20@gmail.com>
2021-05-06 21:16:37 +1000

    commit

--- flaskapp.py
+++ flaskapp.py
@@ -3,7 +3,7 @@ import logging #allow loggings
 import time, sys, json
 from datetime import datetime
 #import yourrobot #import in your own robot functionality --> need to develop
-from camerainterface.py import Camera
+from camera import Camera
 
 '''
 ROBOTENABLED = True #this can be used to disable the robot and still edit the webserver
