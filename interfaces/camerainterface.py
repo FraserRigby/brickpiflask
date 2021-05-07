07a611098f409f47d0382d92f8e3e81fd38c4ecb
Frase Rigby <rigbyf20@gmail.com>
2021-05-07 16:21:55 +1000

    commit

--- interfaces/camerainterface.py
+++ interfaces/camerainterface.py
@@ -4,6 +4,34 @@ License: MIT License
 Attribute: https://github.com/miguelgrinberg/flask-video-streaming/tree/v1
 '''
 
+import io
+import time
+import picamera
+from interfaces.base_camera import BaseCamera
+
+
+class Camera(BaseCamera):
+    @staticmethod
+    def frames():
+        with picamera.PiCamera() as camera:
+            camera.resolution = (320, 240)
+            camera.hflip = True
+            camera.vflip = False
+
+            # let camera warm up
+            time.sleep(2)
+
+            stream = io.BytesIO()
+            for _ in camera.capture_continuous(stream, 'jpeg',
+                                                 use_video_port=True):
+                # return current frame
+                stream.seek(0)
+                yield stream.read()
+
+                # reset stream for next frame
+                stream.seek(0)
+                stream.truncate()
+'''
 import time
 import io
 import threading
@@ -58,3 +86,4 @@ class Camera(object):
                 if time.time() - cls.last_access > 10:
                     break
         cls.thread = None
+'''
