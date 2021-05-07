14df04ba83daaab841adc4a1a34e19ab599ee4c6
Frase Rigby <rigbyf20@gmail.com>
2021-05-07 17:35:16 +1000

    commit

--- interfaces/camerainterface.py
+++ interfaces/camerainterface.py
@@ -14,7 +14,7 @@ class Camera(BaseCamera):
     @staticmethod
     def frames():
         with picamera.PiCamera() as camera:
-            camera.resolution = (640, 480)
+            camera.resolution = (320, 240)
             camera.hflip = True
             camera.vflip = False
 
