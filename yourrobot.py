326e2db8362e6e062150ae6ec5e21339edc8ff41
Frase Rigby <rigbyf20@gmail.com>
2021-05-14 01:09:20 +1000

    commit

--- yourrobot.py
+++ yourrobot.py
@@ -6,7 +6,8 @@ import time
 
 #This class is encapsualtes routines which execute the elementary functions established in the RobotInterface object
 class Robot(RobotInterface):
-    
+
+###----------YOURROBOT SETUP----------###    
     def __init__(self):
         super().__init__()
         self.CurrentRoutine = 'ready' #could be useful to keep track of the current routine
@@ -15,6 +16,32 @@ class Robot(RobotInterface):
     #gets the current routine
     def get_current_routine(self):
         return self.CurrentRoutine
+
+
+###----------SEMI AUTO----------###
+#Look left
+
+#Look right
+
+#Search front
+
+#Search forward
+
+#Search backward
+
+#Firing solution
+
+#Extinguish fire
+
+
+###----------FULL AUTO----------###
+#Full auto
+# -look left
+# --search forward
+# -search front
+# -look right
+# --search backward
+# *if fire detected extinguish, using generated firing solution
 
 #--------------------------------------------------------------------
 #Only execute if this is the main file, this section is good for testing code
