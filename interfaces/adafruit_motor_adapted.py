009f750a61124e30be56f3a1c212de465055aecb
Frase Rigby <rigbyf20@gmail.com>
2021-05-18 12:43:43 +1000

    commit

--- interfaces/adafruit_motor_adapted.py
+++ interfaces/adafruit_motor_adapted.py
@@ -119,8 +119,6 @@ class Servo(_BaseServo):
         if value is None:
             raise ValueError("Continuous servos cannot spin freely")
         self.fraction = (value + 1) / 2
-        if value == 0:
-            self._pwm_out = 0
 
     def __enter__(self):
         return self
